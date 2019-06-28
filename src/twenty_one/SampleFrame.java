ad01b4c107f61840996dbf0e699b168a86bbcd0b
vampire.c <oncwnuPQrpGoY22P2XerA-SaPupg@git.weixin.qq.com>
2019-06-28 10:59:17 +0800

    Initial commit

---
+++ src/twenty_one/SampleFrame.java
@@ -0,0 +1,57 @@
+package twenty_one;
+
+import java.applet.Applet;
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+
+public class SampleFrame extends Frame{
+    SampleFrame(String title){
+        super(title);
+        MyWindowAdapter adapter=new MyWindowAdapter(this);
+        addWindowListener( adapter);
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        g.drawString("frame window",10,40);
+    }
+
+    private class MyWindowAdapter extends WindowAdapter{
+        SampleFrame sampleFrame;
+        public MyWindowAdapter(SampleFrame sampleFrame) {
+            this.sampleFrame=sampleFrame;
+        }
+        @Override
+        public void windowClosed(WindowEvent e) {
+            sampleFrame.setVisible(false);
+        }
+    }
+
+    public class AppletFrame extends Applet{
+        Frame f;
+
+        @Override
+        public void init() {
+            f=new SampleFrame("A frame window");
+            f.setSize(250,250);
+            f.setVisible(true);
+        }
+
+        @Override
+        public void start() {
+            f.setVisible(true);
+        }
+
+        @Override
+        public void stop() {
+            f.setVisible(false);
+        }
+
+        @Override
+        public void paint(Graphics g) {
+            g.drawString("applet window",10,20);
+        }
+    }
+}
